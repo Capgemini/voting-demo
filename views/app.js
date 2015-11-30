7cbdfc64b980dcf9ca872002452604408b11404a
enxebre <alberto.garcial@hotmail.com>
2015-11-30 15:58:18 +0000

    Football teams.

--- views/app.js
+++ views/app.js
@@ -6,8 +6,8 @@ var bg2 = document.getElementById('background-stats-2');
 
 app.controller('statsCtrl', function($scope){
 
-  $scope.option_a = 'Cats';
-  $scope.option_b = 'Dogs';
+  $scope.option_a = 'Arsenal';
+  $scope.option_b = 'Chelsea';
 
   var animateStats = function(percentA, percentB){
     bg1.style.width= percentA+"%";
