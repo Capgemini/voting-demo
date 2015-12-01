f01fddf4735c9685da02b5eab35152cf3522f951
enxebre <alberto.garcial@hotmail.com>
2015-12-01 11:39:59 +0000

    Tesla vs Edison.

--- views/app.js
+++ views/app.js
@@ -6,8 +6,8 @@ var bg2 = document.getElementById('background-stats-2');
 
 app.controller('statsCtrl', function($scope){
 
-  $scope.option_a = 'Apple';
-  $scope.option_b = 'Microsoft';
+  $scope.option_a = 'Tesla';
+  $scope.option_b = 'Edison';
 
   var animateStats = function(percentA, percentB){
     bg1.style.width= percentA+"%";
